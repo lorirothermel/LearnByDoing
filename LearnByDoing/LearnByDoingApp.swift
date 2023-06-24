be8a720c8046c1eb5b84381d8fc8214f9631bac7
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-24 08:23:05 -0400

    Initial Commit

---
+++ LearnByDoing/LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingApp.swift
+//  LearnByDoing
+//
+//  Created by Lori Rothermel on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
